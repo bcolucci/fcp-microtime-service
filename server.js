db9bf3ddf219ce7b65414a4a2e7cb004f17554e4
Brice Colucci <bcolucci@wuha.io>
2016-07-08 10:55:35 +0200

    wip

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 'use strict';
 
-process.on('uncaughtException', function (err) {
-  console.error('Caught exception: %s', err);
-});
-
-// ---
-
 const app = require('express')();
 
-app.use(require('./lib/router'));
+process.on('uncaughtException', err => console.error('Caught exception: %s', err));
 
-app.listen(process.env.PORT, function () {
-  console.log('Server listening on :%d', process.env.PORT);
-});
+app.use(require('./lib/router'));
+app.listen(process.env.PORT, () => console.log('Server listening on :%d', process.env.PORT));
--- test/microtime.js
+++ test/microtime.js
@@ -3,28 +3,30 @@
 const sinon = require('sinon')
   , microtime = require('..');
 
-describe('Should converts unix timestamp / natural date', function () {
+describe('Should converts unix timestamp / natural date', () => {
 
   const NOW = Date.now();
 
-  before(function () { sinon.stub(Date, 'now').returns(NOW); });
-  after(function () { Date.now.restore(); });
+  before(() => {
+    sinon.stub(Date, 'now').returns(NOW);
+  });
+  after(() => {
+    Date.now.restore();
+  });
 
-  it('should only accepts a string', function () {
-    const assertInvalidType = function (fn) { fn.should.throw(microtime.INVALID_TYPE_ERROR); };
+  it('should only accepts a string', () => {
+    const assertInvalidType = fn => fn.should.throw(microtime.INVALID_TYPE_ERROR);
     [ null, undefined, NaN, 42, 42.0, [], {} ]
-      .forEach(function (value) { assertInvalidType(() => microtime.convert(value)); });
+      .forEach(value => assertInvalidType(() => microtime.convert(value)));
     microtime.convert('21, Mar 2016').should.be.ok;
   });
 
-  it('should returns now timestamp if string is empty', function () {
-    const assertNull = function (value) {
-      microtime.convert(value).should.be.deepEqual(microtime.NULL_OUTPUT);
-    };
+  it('should returns now timestamp if string is empty', () => {
+    const assertNull = value => microtime.convert(value).should.be.deepEqual(microtime.NULL_OUTPUT);
     [ '', ' ', '\n' ].forEach(assertNull);
   });
 
-  it('should converts from natural format', function () {
+  it('should converts from natural format', () => {
     const myDateStr = 'Sat Mar 21 2015';
     microtime.convert(myDateStr).should.be.deepEqual({
       natural: myDateStr,
@@ -32,7 +34,7 @@ describe('Should converts unix timestamp / natural date', function () {
     });
   });
 
-  it('should converts from timestamp format', function () {
+  it('should converts from timestamp format', () => {
     const myDateStr = 'Tue Sep 27 1988'
       , myDateTimestampStr = String(new Date(myDateStr).getTime());
     microtime.convert(myDateTimestampStr).should.be.deepEqual({
